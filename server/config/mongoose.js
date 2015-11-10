5d98a6576b9ce9d2d042c87c8ee82dc41bfd9609
Michael J. Hanifan <michael.hanifan@gmail.com>
2015-11-09 22:46:48 -0800

    fixing file pathing

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -4,7 +4,7 @@ var keys = require(__dirname+"/keys.js");
 
 mongoose.connect(keys.remote_db);
 
-var models_path=__dirname+'./../models';
+var models_path=__dirname+'/../models';
 fs.readdirSync(models_path).forEach(function(file){
 	if(file.indexOf('.js')>=0){
 		require(models_path+'/'+file);
