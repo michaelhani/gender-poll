3927a06d09413f1a1ad46b97179c11468c98a471
Michael J. Hanifan <michael.hanifan@gmail.com>
2015-11-09 23:27:05 -0800

    minor logic change to display zero before first vote is cast

--- client/js/controllers/main.controller.js
+++ client/js/controllers/main.controller.js
@@ -1,11 +1,14 @@
 app.controller("mainController", function($scope, mainFactory){
 	var mainCtrl = this;
-	mainCtrl.males=0;
-	mainCtrl.females=0;
 	mainCtrl.getPop = function(){
 		mainFactory.getPop(function(data){
-			mainCtrl.males = data.male/(data.male+data.female);
-			mainCtrl.females = data.female/(data.male+data.female);
+			if(data.male+data.female===0){
+				mainCtrl.males = 0;
+				mainCtrl.females =0;
+			}else{
+				mainCtrl.males = data.male/(data.male+data.female);
+				mainCtrl.females = data.female/(data.male+data.female);
+			}
 		})
 	};
 	mainCtrl.vote = function(gender){
