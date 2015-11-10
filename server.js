c37318d636e1023f97796987d2d5c22eebc14efd
Michael J. Hanifan <michael.hanifan@gmail.com>
2015-11-09 22:38:49 -0800

    adding db info for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.static(__dirname+"/client"));
 require("./server/config/mongoose.js");
 require('./server/config/routes.js')(app);
-var port=8000;
+var port= process.env.PORT || 8000;
 app.listen(port, function(){
 	console.log("------------------");
 	console.log("-------"+port+"-------");
---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	local_db: "mongodb://localhost/genderpoll",
+	remote_db: "mongodb://admin:g3nd3rp0ll@ds053184.mongolab.com:53184/heroku_zjb0n74r"
+}
